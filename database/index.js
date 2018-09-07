31bb6422ea4459a42a806a07cbc46a4d335b43fb
Chang Lee <cleeucla@gmail.com>
2018-09-07 10:31:59 -0700

    add json data for countries and users

--- database/index.js
+++ database/index.js
@@ -10,4 +10,13 @@ db.once('open', () => {
   console.log('Successfully connected to mongoDB');
 });
 
-let userSchema
+let userSchema = new Schema({
+  email: { type: String, unique: true, require: true },
+  password: { type: String, unique: true, require: true },
+  name: String
+});
+
+let locationSchema = new Schema({
+  city: String,
+  country: String
+});
