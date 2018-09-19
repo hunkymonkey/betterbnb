b79f43dc366d574e83662bb89fcec2343819b5e4
Chang Lee <cleeucla@gmail.com>
2018-09-18 18:40:35 -0700

    add CORS

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const routes = require('./routes.js');
 const PORT = 8000;
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 app.use(express.static(__dirname + '/../client/dist'));
 
 app.use('/api', routes);
