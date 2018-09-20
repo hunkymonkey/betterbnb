6353d048738e032e34732a46130058aa8115c049
Chang Lee <cleeucla@gmail.com>
2018-09-19 19:10:04 -0700

    add package-lock to gitignore and add path to server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 const cors = require('cors');
 const routes = require('./routes.js');
 const PORT = 8000;
@@ -8,7 +9,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
-app.use(express.static(__dirname + '/../client/dist'));
+app.use(express.static(path.join(__dirname, "/../client/dist")));
 
 app.use('/api', routes);
 
