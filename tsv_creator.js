a950552593bd6e7cd2e32bdce4685c06624e44e4
Chang Lee <cleeucla@gmail.com>
2018-09-26 17:00:10 -0700

    add json data

--- tsv_creator.js
+++ tsv_creator.js
@@ -1,18 +1,25 @@
+const fs = require('fs');
+const imgLinkArr = require('./img_link_data.js');
+const { random7 } = require('./database/models.js');
+
 const arr = [];
-const randomImg = () => Math.floor(Math.random() * 1050) + 1;
-const randomDimension = () => Math.floor(Math.random() * 801) + 200;
 
 const genRandomImages = () => {
-  for (let i = 0; i < 1000; i++) {
-    let tempArr = [];
-    arr.push(
-      `{
-        https://picsum.photos/${randomDimension()}/${randomDimension()}?image=${randomImg()}
-      }`
-    );
+  for (let i = 0; i < 100000; i++) {
+    let pictures = random7(imgLinkArr);
+    let tempObj = {
+      'houseId': i,
+      'pictures': pictures
+    };
+    arr.push(JSON.stringify(tempObj));
   }
 };
 
 genRandomImages();
 
+fs.writeFile('json_data.json', arr, err => {
+  if (err) {
+    console.log('error writing to json_data');
+  }
+});
 
